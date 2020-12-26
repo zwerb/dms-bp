84b02cb68232dafdc53470b35023e5782fe73d49
Zwerb <sy@zwerb.com>
2020-12-26 13:33:38 -0800

    styling for view frames

--- client/app.js
+++ client/app.js
@@ -8,12 +8,18 @@ const App = () => {
     <div className="main-wrapper">
       <header className="header">
         <div className="header-branding">
-          <img className="header-image" src="images/zwerb-z_logo-WHT_BDR.png" />DMS
+          <img className="header-image" src="images/zwerb-z_logo-WHT_BDR.png" />
+          DMS
         </div>
         <Navbar />
       </header>
       <hr />
-      <Routes />
+      <content className="main">
+        <Routes />
+      </content>
+      <aside className="aside aside-1">Aside 1</aside>
+      <aside className="aside aside-2">Aside 2</aside>
+      <footer className="footer">Footer</footer>
     </div>
   )
 }
--- client/routes.js
+++ client/routes.js
@@ -18,18 +18,14 @@ class Routes extends Component {
 
     return (
       <Switch>
-        {/* Routes placed here are available to all visitors */}
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
-        <Route path="/upload" component={ImageUploadForm} />
         {isLoggedIn && (
           <Switch>
-            {/* Routes placed here are only available after logging in */}
             <Route path="/home" component={UserHome} />
+            <Route path="/upload" component={ImageUploadForm} />
           </Switch>
         )}
-        {/* Displays our Login component as a fallback */}
-        <Route component={Login} />
       </Switch>
     )
   }
